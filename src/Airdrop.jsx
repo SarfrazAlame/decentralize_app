133bb2fbc926084e8fe9f6de8e3e7169189d6b64
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-12-16 08:43:45 +0530

    connect phantom

---
+++ src/Airdrop.jsx
@@ -0,0 +1,10 @@
+function AirDrop() {
+    return (
+        <div>
+            <input type="text" placeholder="Amount" />
+            <button>Send Airdrop</button>
+        </div>
+    )
+}
+
+export default AirDrop
